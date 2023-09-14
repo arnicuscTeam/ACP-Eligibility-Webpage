4fc36479289ba595cf6dc9bcd5804cfe10410170
Angel Chavez-Penate <chavezpe@usc.edu>
2023-09-13 19:04:32 -0700

    Testing map data

--- WebPage.py
+++ WebPage.py
@@ -2,8 +2,8 @@ import subprocess
 from Code.acs_pums import determine_eligibility
 import pandas as pd
 import streamlit as st
-# import matplotlib.pyplot as plt
-# import geopandas as gpd
+import matplotlib.pyplot as plt
+import geopandas as gpd
 
 
 # Set page name
